9ebe9d8a726fedac7b78a1d2fd9a49794bd5bf05
stvort <stvort@mail.ru>
2025-11-03 16:18:08 +0400

    internal tomcat demo updated + refactoring

--- internal-server-app/src/main/java/ru/otus/Application.java
+++ internal-server-app/src/main/java/ru/otus/Application.java
@@ -1,10 +1,10 @@
 package ru.otus;
 
 import com.google.gson.GsonBuilder;
-import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Context;
 import org.apache.catalina.servlets.DefaultServlet;
 import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.JarResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
 import ru.otus.controller.AstrolabeRestController;
 import ru.otus.controller.ReportsRestController;
@@ -15,29 +15,22 @@ import ru.otus.services.DelegatingReportService;
 import ru.otus.services.SextantReportService;
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Locale;
 
 public class Application {
-    public static void main(String[] args) throws LifecycleException {
+    public static void main(String[] args) throws Exception {
         var tomcat = new Tomcat();
         tomcat.setBaseDir("internal-server-app/temp");
         tomcat.setPort(8080);
 
         var contextPath = "/app";
-
-        var docBase = new File("internal-server-app/src/main/webapp").getAbsolutePath();
-        var context = tomcat.addContext(contextPath, docBase);
-
+        var context = prepareContext(tomcat, contextPath);
         context.addWelcomeFile("index.html");
-        var resources = new StandardRoot(context);
-        resources.addPreResources(new DirResourceSet(resources, "/",
-                docBase, "/"));
-
-        context.setResources(resources);
-
-        var commercialService = new CommercialService();
 
         var gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
+        var commercialService = new CommercialService();
         var astrolabeReportService = new AstrolabeReportService(commercialService);
         var sextantReportService = new SextantReportService(commercialService);
         var reportService = new DelegatingReportService(List.of(astrolabeReportService,
@@ -47,7 +40,7 @@ public class Application {
         var astrolabeRestController = new AstrolabeRestController(gson, commercialService);
         var sextantRestController = new SextantRestController(gson, commercialService);
 
-        var defaultControllerWrapper = Tomcat.addServlet(context, "default", new DefaultServlet());
+        var defaultControllerWrapper = tomcat.addServlet(contextPath, "default", new DefaultServlet());
         var reportsRestControllerWrapper = tomcat.addServlet(contextPath, "ReportsRestController",
                 reportsRestController);
         var astrolabeRestControllerWrapper = tomcat.addServlet(contextPath, "AstrolabeRestController",
@@ -64,5 +57,26 @@ public class Application {
         tomcat.getConnector();
         System.out.println("http://localhost:8080/app");
         tomcat.getServer().await();
+    }
+
+    private static Context prepareContext(Tomcat tomcat, String contextPath) throws Exception {
+        File jarFile = new File(Application.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+        String jarPath = jarFile.getAbsolutePath();
+        boolean isNotInJar = !jarPath.toLowerCase(Locale.ROOT).endsWith(".jar");
+        System.out.println(jarPath);
+
+        if (isNotInJar) {
+            var docBase = Paths.get(jarPath, "static").toAbsolutePath().toString();
+            return tomcat.addContext(contextPath, docBase);
+        }
+
+        var docBase = new File(".").getAbsolutePath();
+        var context = tomcat.addContext(contextPath, docBase);
+        StandardRoot root = new StandardRoot(context);
+        JarResourceSet jarResourceSet = new JarResourceSet(root, "/", jarPath, "/static");
+        root.addJarResources(jarResourceSet);
+        context.setResources(root);
+
+        return context;
     }
 }
